50964dbca982f12a28bac83152439bd3f09cb103
Gabriel Matei <gabrielvmatei@gmail.com>
2023-02-05 14:02:00 +0200

    fix referral rewards

--- src/pages/Referral/hooks/useGetReferralRewards.ts
+++ src/pages/Referral/hooks/useGetReferralRewards.ts
@@ -7,7 +7,9 @@ export const useGetReferralRewards = () => {
   const { data, refetch } = useQuery(REFERRAL_INFO);
 
   return React.useMemo(() => {
-    const rewards = data?.referralRewards as ReferralRewards | undefined;
+    const rewards = data?.accountReferralInfo?.referralRewards as
+      | ReferralRewards
+      | undefined;
 
     return {
       rewards,
